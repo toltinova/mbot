73b3b43eace3b57fc2d0e89dc0f6cee8af33c1d2
Olya Toltinova <nyanpost@gmail.com>
2021-05-11 22:28:56 +0300

    WIP

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,27 @@ var app = express();
 app.get('/', function(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ a: 1 }));
+  res.end(JSON.stringify({ a: "Welcome to MBot" }));
 });
 
-app.get('/hello', function(req, res) {
+app.get('/stats', function(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ a: 2 }));
+  res.end(JSON.stringify({ "a":"BTC",
+  "b":"ETH"}));
+});
+
+
+app.get('/stats/btc', function(req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ "a": "BTC" }));
+});
+
+app.get('/total-income', function(req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ a: 1000000000 }));
 });
 
 app.listen(3000);
