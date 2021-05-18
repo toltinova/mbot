1695400b2c34e8a829a105427003da47398af50d
Olya Toltinova <nyanpost@gmail.com>
2021-05-18 22:20:19 +0300

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TradesComponent } from './trades/trades.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'trades', component: TradesComponent }
+  { path: 'trades', component: TradesComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
