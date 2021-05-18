05f25f95145fa64483eda87ed506b89e0657e5d6
Olya Toltinova <nyanpost@gmail.com>
2021-05-18 21:41:13 +0300

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { DiagramComponent } from './diagram/diagram.component';
 import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
     FooterComponent,
     DiagramComponent,
     HeaderComponent,
+    DashboardComponent,
 
   ],
   imports: [
