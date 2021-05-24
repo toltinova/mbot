dc436a6c83036e2d43d83b7b4a2fb63eddec6827
Olya Toltinova <nyanpost@gmail.com>
2021-05-24 18:28:25 +0300

    WIP

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,31 @@
-'use strict';
-
-const fs = require('fs');
-
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var port = 3000;
 var message = 'Server is running on port '+ port;
 
-
 app.get('/', function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ a: "Welcome to MBot" }));
+  configureResponse(res, 'welcome-message.json');
 });
 
 app.get('/stats', function(req, res) {
   configureResponse(res, 'stats.json');
 });
 
-
 app.get('/stats/btc', function(req, res) {
   configureResponse(res, 'stats-btc.json');
 });
 
 app.get('/total-income', function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ a: 1000000000 }));
+  configureResponse(res,'total-income.json')
 });
 
 function configureResponse(res, fileName) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  let rawdata = fs.readFileSync(fileName);
-  let data = JSON.parse(rawdata);
-  res.end(JSON.stringify(data));
-  console.log(data);
+  var data = fs.readFileSync(fileName);
+  var json = JSON.parse(data);
+  res.end(JSON.stringify(json));
 }
 
 app.listen(port);
