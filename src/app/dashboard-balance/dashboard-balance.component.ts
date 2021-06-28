54f1171489e36508f4030abf343ba9d344e7f0fe
Olya Toltinova <nyanpost@gmail.com>
2021-06-29 00:18:29 +0300

    WIP

--- src/app/dashboard-balance/dashboard-balance.component.ts
+++ src/app/dashboard-balance/dashboard-balance.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiService } from '../api.service';
 import { TotalBalance } from './total-balance';
 import { catchError, map, tap } from 'rxjs/operators';
@@ -8,17 +8,26 @@ import { catchError, map, tap } from 'rxjs/operators';
   templateUrl: './dashboard-balance.component.html',
   styleUrls: ['./dashboard-balance.component.css']
 })
-export class DashboardBalanceComponent implements OnInit {
+export class DashboardBalanceComponent implements OnInit, OnDestroy {
 
-   totalBalance : TotalBalance;
+  totalBalance : TotalBalance;
+  interval : any;
 
-   constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) { }
 
-   ngOnInit(): void {
-      this.apiService.getTotalBalance().subscribe({
-         next: body => { this.totalBalance = body; },
-         error: error => { console.error('There was an error!', error); }
-      })
-   }
+  ngOnInit(): void {
+    this.interval = setInterval(() => { this.loadData(); }, 2500);
+    this.loadData();
+  }
 
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  loadData(): void {
+    this.apiService.getTotalBalance().subscribe({
+        next: body => { this.totalBalance = body; },
+        error: error => { console.error('There was an error!', error); }
+    });
+  }
 }
